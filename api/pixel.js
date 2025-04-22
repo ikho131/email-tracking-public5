df089ed9940752f0c24bf3f8a9c5d9349e39c666
ikho131 <ikho.3line@gmail.com>
2025-04-23 07:38:09 +0900

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { email, university, company, type, t } = req.query;
 
-  const scriptUrl = "https://script.google.com/macros/s/AKfycbxIubsPa2Y1ICCY_z_MgVHVs73fDTGC7g4QKldRbF6JW6nH7dy6QudLi6bejKowZ9JI/exec";
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwEBA4PwO8jiskRxHxTFg6W4nz7qEfvHWzh63_AyqoDrWkQEQvIkIzpuTeWaqGV2Ese/exec";
 
   try {
     await fetch(scriptUrl, {
